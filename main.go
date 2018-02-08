2f35c76d15acc1b03d1fbe5e9b1c7783884c3673
FlakM <maciej.jan.flak@gmail.com>
2018-02-08 11:29:57 +0100

    modified to use new go-gitlab

--- main.go
+++ main.go
@@ -6,17 +6,20 @@ import (
 	"log"
 	"os"
 
-	"github.com/xanzy/go-gitlab"
+	gitlab "github.com/xanzy/go-gitlab"
 )
 
 var (
-	token *string = flag.String("token", os.Getenv("GITLAB_TOKEN"), "api token with admin access")
+	token  = flag.String("token", os.Getenv("GITLAB_TOKEN"), "api token with admin access")
+	dryRun = flag.Bool("dryRun", true, "if set to true only prints jobs")
 )
 
 func main() {
 	flag.Parse()
 
 	git := gitlab.NewClient(nil, *token)
+
+	fmt.Printf("Dry run set to %t\n", *dryRun)
 
 	runners, _, err := git.Runners.ListRunners(nil, nil)
 
@@ -32,54 +35,41 @@ func main() {
 		}
 	}
 
+	fmt.Println("total runners", len(runners))
+	fmt.Println("inactive runners ", len(inactiveRunners))
+
 	visibility := gitlab.PrivateVisibility
 	options := gitlab.ListProjectsOptions{ListOptions: gitlab.ListOptions{Page: 0, PerPage: 10}, Visibility: &visibility}
 	projects, _, err := git.Projects.ListProjects(&options, nil)
 
 	for _, project := range projects {
 		p := *project
-		fmt.Printf("clearing runners for project %s\n", p.Name)
+		fmt.Printf("disabling runners for project %s\n", p.Name)
 		for _, r := range inactiveRunners {
-			_, err := disableRunner(git, r, project)
-			if err != nil {
-				fmt.Printf("failed to disable runner %s due to %s\n", r.Description, err)
+			if *dryRun {
+				fmt.Printf("would disable runner %d for project %d\n", r.ID, p.ID)
+			} else {
+				git.Runners.DisableProjectRunner(p.ID, r.ID, nil)
+				if err != nil {
+					fmt.Printf("failed to disable runner %s due to %s\n", r.Description, err)
+				} else {
+					fmt.Printf("Runner %d disabled for project %d [%s]\n", r.ID, p.ID, p.Name)
+				}
 			}
 		}
 	}
 
 	for _, r := range inactiveRunners {
-		_, err := deleteRunner(git, r)
-		if err != nil {
-			fmt.Printf("failed to delete runner %s due to %s\n", r.Description, err)
+		if *dryRun {
+			fmt.Printf("would remove runner %d with description %s\n", r.ID, r.Description)
+		} else {
+			fmt.Printf("removing runner with id %d\n", r.ID)
+			_, err := git.Runners.RemoveRunner(r.ID, nil)
+			if err != nil {
+				fmt.Printf("failed to delete runner %s due to %s\n", r.Description, err)
+			} else {
+				fmt.Printf("deleted runner %s\n", r.Description)
+			}
 		}
 	}
-
-	fmt.Println("total runners len:", len(runners))
-	fmt.Println("inactive runners len:", len(inactiveRunners))
-
-}
-
-func disableRunner(c *gitlab.Client, r *gitlab.Runner, p *gitlab.Project) (bool, error) {
-	// /projects/:id/runners/:runner_id
-	uri := fmt.Sprintf("/projects/%d/runners/%d", p.ID, r.ID)
-	req, err := c.NewRequest("DELETE", uri, nil, nil)
-	var rs interface{}
-	rsp, err := c.Do(req, &rs)
-	if err != nil {
-		return false, err
-	}
-	log.Printf("Got rsp %d with msg: %s for project %s and runner %s\n", rsp.StatusCode, rsp.Status, p.Name, r.Description)
-	return rsp.StatusCode == 200, nil
-}
-
-func deleteRunner(c *gitlab.Client, r *gitlab.Runner) (bool, error) {
-	id := fmt.Sprintf("runners/%d", r.ID)
-	req, err := c.NewRequest("DELETE", id, nil, nil)
-	var rs interface{}
-	rsp, err := c.Do(req, &rs)
-	if err != nil {
-		return false, err
-	}
-
-	return rsp.StatusCode == 200, nil
 }
